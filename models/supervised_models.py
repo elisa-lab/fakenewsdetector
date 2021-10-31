0511c51a3ccf2bc4e9b3e3dfcba62682f8ddd5bd
aghosh <aniruddha_ghosh@apple.com>
2021-10-31 21:01:57 +0000

    added abstract class for classifier

---
+++ models/supervised_models.py
@@ -0,0 +1,22 @@
+class classifier:
+    '''
+    Abstract class for traditional supervised models
+    '''
+
+    def add_train_file(self, train_file):
+        raise NotImplementedError()
+    def add_test_file(self, test_file):
+        raise NotImplementedError()
+
+    def load_data(self):
+        raise NotImplementedError()
+    def prepare_data(self):
+        raise NotImplementedError()
+    def train_model(self):
+        raise NotImplementedError()
+    def predict_model(self):
+        raise NotImplementedError()
+    def save_model(self, model_path):
+        raise NotImplementedError()
+    def load_model(self, model_path):
+        raise NotImplementedError()

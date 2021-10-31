6848e24a1ecdb5782df310ca0cec26143d3410a7
aghosh <aniruddha_ghosh@apple.com>
2021-10-31 20:53:36 +0000

    added ngram featurizations

--- fakenewsdetector.py
+++ fakenewsdetector.py
@@ -1,11 +1,14 @@
 from pathlib import Path
-from preprocessing.dataprocessor import read_data
+from preprocessing.dataprocessor import read_data, build_ngram_features
 
 if __name__=='__main__':
 
     train_path = Path('datafiles/datafake_train.csv')
     test_path = Path('datafiles/datafake_test.csv')
     train_data, test_data = read_data(train_path, test_path)
+    vectorizer = build_ngram_features(train_data)
+
+
 
 
     print('Data loaded')
--- preprocessing/dataprocessor.py
+++ preprocessing/dataprocessor.py
@@ -2,9 +2,18 @@
 This script will read the input data and preprocess it. Also, it will covert into features.
 """
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+
 
 def read_data(train_file, test_file):
     train_data = pd.read_csv(train_file, delimiter=';')
     test_data = pd.read_csv(test_file, delimiter=';')
 
     return train_data, test_data
+
+def build_ngram_features(train_data):
+    final_stopwords_list = stopwords.words('french')
+    vectorizer = CountVectorizer(analyzer='word', ngram_range=(1,3), stop_words=final_stopwords_list)
+    vectorizer.fit(train_data['post'])
+    return vectorizer

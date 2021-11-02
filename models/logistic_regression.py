0d3a0094f6a5829836334f787f275f1b84414fa7
Aniruddha Ghosh <arghyaonline@gmail.com>
2021-11-02 17:46:45 +0000

    Data featurise (#2)
    * adding load data function
    
    * Testing load data function
    
    * init file
    
    * added ngram featurizations
    
    * added abstract class for classifier
    
    * init file
    
    * addition of logistic regression model
    
    * added comments
    
    * bug fix
    
    * added comments
    
    * addressed the PR comments
    
    * added typing
    
    * added typing
    
    Co-authored-by: aghosh <aniruddha_ghosh@apple.com>

---
+++ models/logistic_regression.py
@@ -0,0 +1,111 @@
+from models.supervised_models import Classifier
+from preprocessing.dataprocessor import read_data, build_ngram_features
+from sklearn.linear_model import LogisticRegression
+import joblib
+from pathlib import Path
+from sklearn.metrics import classification_report
+from typing import List
+
+
+class LogisticRegressionClassifier(Classifier):
+    """
+    Logistic regression classifier
+
+    train_files: Holds the list train file names
+    test_files: Holds the list test file names
+    train_data: Holds the entire train data
+    test_data: Holds the entire test data
+    features: Contains the features for the model
+    labels: contains the output label
+    vectorizer: vectorizer to extract ngram features.
+
+    """
+
+    def __init__(self):
+        self.train_files = []
+        self.test_files = []
+        self.train_data = None
+        self.test_data = None
+
+        self.features = None
+        self.labels = None
+
+        self.vectorizer = None
+
+    def add_train_file(self, train_file: Path) -> None:
+        """
+        Adds a train file to the train file list
+
+        :param train_file: Path of the train file
+        """
+        self.train_files.append(train_file)
+
+    def add_test_file(self, test_file: Path) -> None:
+        """
+        Adds a test file to the test file list
+
+        :param test_file: Path of the test file
+        """
+        self.test_files.append(test_file)
+
+    def load_data(self) -> None:
+        """
+        Loads the train and test data
+        """
+        train_data, test_data = read_data(self.train_files, self.test_files)
+        self.train_data = train_data
+        self.test_data = test_data
+
+    def prepare_data(self) -> None:
+        """
+        Extracts the features from the dataset (ngram and more)
+        """
+        self.vectorizer = build_ngram_features(self.train_data)
+
+        # convert training corpus in to vectors
+        self.features = self.vectorizer.transform(self.train_data['post']).toarray()
+        # extracting the output labels
+        self.labels = self.train_data['fake']
+
+    def train_model(self) -> None:
+        """
+        Trains a model using the prepared dataset
+        """
+        self.model = LogisticRegression()
+        self.model.fit(self.features, self.labels)
+
+    def save_model(self, model_path: Path) -> None:
+        """
+        Saves the trained model
+
+        :param model_path: Path of the dictionary where the model will be saved
+        """
+        joblib.dump(self.model, Path(model_path / 'lr.joblib.pkl'), compress=9)
+
+    def load_model(self, model_path: Path) -> None:
+        """
+        Loads the trained model
+
+        :param model_path: Path of the dictionary from where the model will be loaded
+        """
+        self.model = joblib.load(Path(model_path / 'lr.joblib.pkl'))
+
+    def predict_model(self) -> List:
+        """
+        Predicts the output of the test data
+
+        :return: Returns predictions on the test data by the model
+        """
+        features = self.vectorizer.transform(self.test_data['post'])
+        predictions = self.model.predict(features)
+
+        return predictions
+
+    def calculate_accuracy(self) -> None:
+        """
+        Calculate accuracy on the test data
+        """
+        y_pred = self.predict_model()
+        y_true = self.test_data['fake']
+
+        print(classification_report(y_true=y_true, y_pred=y_pred))
---
+++ preprocessing/dataprocessor.py
@@ -0,0 +1,44 @@
+"""
+This script will read the input data and preprocess it. Also, it will covert into features.
+"""
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+from typing import List, Tuple
+
+
+def read_data(train_files: List, test_files: List) -> Tuple[pd.Dataframe, pd.Dataframe]:
+    """
+    Reads the csv data into Pandas dataframe
+
+    :param train_files: List of train files
+    :param test_files: List of test files
+    :return: Returns two dataframes (train and test)
+    """
+    train_data = []
+    for train_file in train_files:
+        train_data.append(pd.read_csv(train_file, delimiter=';'))
+
+    train_data = pd.concat(train_data, ignore_index=True)
+
+    test_data = []
+    for test_file in test_files:
+        test_data.append(pd.read_csv(test_file, delimiter=';'))
+
+    test_data = pd.concat(test_data, ignore_index=True)
+
+    return train_data, test_data
+
+
+def build_ngram_features(train_data: pd.Dataframe) -> CountVectorizer:
+    """
+    Initializes CountVectorizer
+
+    :param train_data: Train data to use in CountVectorizer
+    :return: Returns an instance of CountVectorizer
+    """
+    final_stopwords_list = stopwords.words('french')
+    vectorizer = CountVectorizer(analyzer='word', ngram_range=(1, 3), stop_words=final_stopwords_list,
+                                 max_features=5000)
+    vectorizer.fit(train_data['post'])
+    return vectorizer
